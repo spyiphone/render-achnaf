c95d8f66172d6a09e0d08f7d0cfd0acf6d79c085
ass <jurus32@gmail.com>
2025-09-06 23:55:48 +0700

    india ig 1

--- public/instagram/test_3.py
+++ public/instagram/test_3.py
@@ -7,7 +7,7 @@ L = instaloader.Instaloader()
 SESSIONID = "77120680498%3A4JFZeViihgW7FK%3A26%3AAYcNesCTFEmNKRXhqhs1C3e26xtaA97EEN1X3hU_eQ"
 L.context._session.cookies.set("sessionid", SESSIONID)
 
-with open("ig-ng.json", "r", encoding="utf-8") as f:
+with open("ig-in.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
 for user in data:
@@ -18,7 +18,7 @@ for user in data:
     except Exception as e:
         print(f"Error {user['name']}: {e}")
 
-with open("ig-ng_updated.json", "w", encoding="utf-8") as f:
+with open("ig-in_updated.json", "w", encoding="utf-8") as f:
     json.dump(data, f, indent=2, ensure_ascii=False)
 
 print("✅ Selesai update foto profil HD")
